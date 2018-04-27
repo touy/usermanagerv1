6b3ff62bd58e30720cb190641670b7d307b9afc2
anousone <touya_ra@hotmail.com>
2018-04-27 16:49:03 +0700

    fix res is not define res.username to user.username

--- app.js
+++ app.js
@@ -1378,7 +1378,7 @@ function login_ws(js) {
         authentication(js.client.data.user).then(function (user) {
             console.log('authen res');
             //console.log(res);        
-            r_client.get('_online_'+res.username,(err,res)=>{
+            r_client.get('_online_'+user.username,(err,res)=>{
                 let online=JSON.parse(res);
                 if(err){
                     js.client.data.message = err;
