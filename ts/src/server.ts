ae2c6c4cd14023974d3d15ee99354b1350528d99
touy <touya.ra@gmail.com>
2022-07-12 17:44:19 +0700

    update usermanager 1

--- ts/src/server.ts
+++ ts/src/server.ts
@@ -1,9 +1,3 @@
 import server from './app';
 const port = Number(process.env.PORT) || 6688;
-server.listen(port, (err) => {
-    if (err) {
-        return console.log(err);
-    }
-  
-    return console.log(`server is listening on ${port}`);
-  });
+server.listen(port);
